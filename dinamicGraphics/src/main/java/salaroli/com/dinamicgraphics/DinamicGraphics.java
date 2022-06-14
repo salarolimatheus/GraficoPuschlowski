7e9386a732632dacad4cd259d6e0ac5593510ca9
Matheus Salaroli <mat.s@terra.com.br>
2022-06-14 12:23:12 -0300

    Biblioteca Adicionada

---
+++ dinamicGraphics/src/main/java/salaroli/com/dinamicgraphics/DinamicGraphics.java
@@ -0,0 +1,14 @@
+package salaroli.com.dinamicgraphics;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DinamicGraphics extends View {
+
+   public DinamicGraphics(Context context, @Nullable AttributeSet attrs) {
+      super(context, attrs);
+   }
+}
