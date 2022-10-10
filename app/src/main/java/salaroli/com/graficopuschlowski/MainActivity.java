ee9fb47060c3036a6cd71a8aa9381ecba23e43ac
Matheus Salaroli <mat.s@terra.com.br>
2022-10-10 10:26:16 -0300

    Titles and tickers update

--- app/src/main/java/salaroli/com/graficopuschlowski/MainActivity.java
+++ app/src/main/java/salaroli/com/graficopuschlowski/MainActivity.java
@@ -1,6 +1,7 @@
 package salaroli.com.graficopuschlowski;
 
 import androidx.appcompat.app.AppCompatActivity;
+import salaroli.com.dinamicgraphics.DinamicGraphics;
 
 import android.os.Bundle;
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DinamicGraphics dinamicGraphics = findViewById(R.id.dinamic_graphics);
+//        String[] Ytickers = new String[] { "90", "100", "110", "120", "130", "140", "150", "160", "170", "180",
+//                "190", "200", "210", "220", "230", "240", "250", "260", "270", "280", "290", "300", "310", "320",
+//                "330", "340", "350", "360"};
+        String[] Ytickers = new String[] { "90", "120", "150", "180", "210", "240", "270", "300", "330", "360"};
+        String[] Xtickers = new String[] {"0", "20", "40", "60", "80", "100", "120", "140", "160", "180"};
+
+        dinamicGraphics.setGradeStatus(true);
+        dinamicGraphics.setXNameTickers(Xtickers);
+        dinamicGraphics.setYNameTickers(Ytickers);
+        dinamicGraphics.setAxisTitles("α(°)", "β(°)");
     }
 }
--- dinamicGraphics/src/main/java/salaroli/com/dinamicgraphics/DinamicGraphics.java
+++ dinamicGraphics/src/main/java/salaroli/com/dinamicgraphics/DinamicGraphics.java
@@ -13,16 +13,15 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class DinamicGraphics extends View {
-   private Paint paintEixos, paintGrade;
-   private Path pathEixos, pathGrade, pathTicker, pathCursor, pathCurvaSelecionada, pathCurvasSecundarias;
+   private Paint paintEixos, paintGrade, paintTitles, paintNameTickers;
+   private Path pathEixos, pathGradeX, pathGradeY, pathTickerX, pathTickerY, pathCursor, pathCurvaSelecionada, pathCurvasSecundarias;
    private Rect rect;
-   private float widthLeft;
-   private float heightTop;
-   private int larguraTotal;
-   private int alturaTotal;
-   private float widthRight;
-   private float heightBottom;
-   private float paddingTicker;
+   private int larguraTotal, alturaTotal;
+   private float widthLeft, widthRight, heightTop, heightBottom, paddingTicker;
+   private boolean gradeStatus;
+   private String xTitle, yTitle;
+   private String[] xNameTickers, yNameTickers;
+   private float sizeTitles, sizeNameTickers;
 
    public DinamicGraphics(Context context) {
       super(context);
@@ -33,6 +32,7 @@ public class DinamicGraphics extends View {
       init();
    }
    private void init() {
+      gradeStatus = true;
       paintEixos = new Paint();
       paintEixos.setStrokeWidth(5);
       paintEixos.setStyle(Paint.Style.STROKE);
@@ -43,13 +43,21 @@ public class DinamicGraphics extends View {
       paintGrade.setStrokeWidth(5);
       paintGrade.setStyle(Paint.Style.STROKE);
       paintGrade.setColor(Color.GRAY);
-      paintGrade.setPathEffect(new DashPathEffect(new float[]{5, 10, 15, 20}, 0));
+      paintGrade.setPathEffect(new DashPathEffect(new float[]{15, 30}, 0));
+//      paintGrade.setPathEffect(new DashPathEffect(new float[]{5, 10, 15, 20}, 0));
       paintGrade.setAntiAlias(true);
 
+      paintTitles = new Paint();
+      paintTitles.setAntiAlias(true);
+
+      paintNameTickers = new Paint();
+      paintNameTickers.setAntiAlias(true);
 
       pathEixos = new Path();
-      pathGrade = new Path();
-      pathTicker = new Path();
+      pathGradeX = new Path();
+      pathGradeY = new Path();
+      pathTickerX = new Path();
+      pathTickerY = new Path();
 
       // AINDA NÃO UTILIZADAS
       pathCursor = new Path();
@@ -60,31 +68,70 @@ public class DinamicGraphics extends View {
    @Override
    protected void onDraw(Canvas canvas) {
       super.onDraw(canvas);
+
       paintEixos.setColor(Color.WHITE);
       canvas.drawRect(rect, paintEixos);
-      canvas.drawPath(pathGrade, paintGrade);
+      if (gradeStatus) {
+         canvas.drawPath(pathGradeX, paintGrade);
+         canvas.drawPath(pathGradeY, paintGrade);
+      }
       paintEixos.setColor(Color.BLACK);
       canvas.drawPath(pathEixos, paintEixos);
-      canvas.drawPath(pathTicker, paintEixos);
+      canvas.drawPath(pathTickerX, paintEixos);
+      canvas.drawPath(pathTickerY, paintEixos);
+
+      drawAxisTitles(canvas);
+      drawNameTickers(canvas);
 
       //DESENHO DAS CURVAS
+   }
+
+   private void drawNameTickers(Canvas canvas) {
+      paintNameTickers.setTextAlign(Paint.Align.CENTER);
+      if (xNameTickers.length > 0) {
+         for (int i = 0; i < xNameTickers.length; i++) {
+            float passo = (widthRight - widthLeft)/(xNameTickers.length-1);
+            canvas.drawText(xNameTickers[i], widthLeft + i * passo, 0.97f * alturaTotal, paintNameTickers);
+         }
+      }
+
+      paintNameTickers.setTextAlign(Paint.Align.RIGHT);
+      if(yNameTickers.length > 0) {
+         for (int i = 0; i < yNameTickers.length; i++) {
+            float passo = (heightBottom - heightTop)/(yNameTickers.length-1);
+            final float heightConstant = heightBottom + (sizeNameTickers * 0.4f);
+            canvas.drawText(yNameTickers[i], (widthLeft*0.8f), heightConstant - i * passo, paintNameTickers);
+         }
+      }
+   }
+   private void drawAxisTitles(Canvas canvas) {
+      paintTitles.setTextAlign(Paint.Align.LEFT);
+      canvas.drawText(yTitle, 0, (heightTop*0.5f), paintTitles);
+      paintTitles.setTextAlign(Paint.Align.RIGHT);
+      canvas.drawText(xTitle, (larguraTotal), (alturaTotal*0.99f), paintTitles);
    }
 
    protected void onSizeChanged(int width, int height, int oldw, int oldh) {
       alturaTotal = height;
       larguraTotal = width;
 
-      heightTop = 0.02f * alturaTotal;
+      heightTop = 0.05f * alturaTotal;
       heightBottom = 0.94f * alturaTotal;
       widthLeft = 0.10f * larguraTotal;
-      widthRight = 0.98f * larguraTotal;
+      widthRight = 0.96f * larguraTotal;
       paddingTicker = 0.01f * larguraTotal;
 
       pathEixos.reset();
-      pathGrade.reset();
+      pathGradeX.reset();
 
       desenhaEixos();
-      desenhaGrade();
+      desenhaGradeX(xNameTickers.length);
+      desenhaGradeY(yNameTickers.length);
+
+      sizeTitles = widthLeft*0.45f;
+      sizeNameTickers = widthLeft*0.4f;
+      paintTitles.setTextSize(sizeTitles);
+      paintNameTickers.setTextSize(sizeNameTickers);
 
       rect = new Rect(0, 0, width, height);
       super.onSizeChanged(width, height, oldw, oldh);
@@ -97,23 +144,48 @@ public class DinamicGraphics extends View {
       pathEixos.lineTo(widthRight, heightTop);
       pathEixos.lineTo(widthLeft, heightTop);
    }
-   private void desenhaGrade() {
-      for (int i = 1; i < 26; i++) {
-         float passo = (heightBottom - heightTop)/26;
-         pathGrade.moveTo(widthLeft, heightTop + i * passo);
-         pathGrade.lineTo(widthRight, heightTop + i * passo);
+   private void desenhaGradeX(int length) {
+      if (length <= 0)
+         return;
+      length -= 1;
+      pathGradeX.reset();
+      for (int i = 1; i < length; i++) {
+         float passo = (widthRight - widthLeft)/length;
+         pathGradeX.moveTo(widthLeft + i * passo, heightTop);
+         pathGradeX.lineTo(widthLeft + i * passo, heightBottom);
 
-         pathTicker.moveTo(widthLeft + paddingTicker, heightTop + i * passo);
-         pathTicker.lineTo(widthLeft - paddingTicker, heightTop + i * passo);
+         pathTickerX.moveTo(widthLeft + i * passo, heightBottom + paddingTicker);
+         pathTickerX.lineTo(widthLeft + i * passo, heightBottom - paddingTicker);
       }
+   }
+   private void desenhaGradeY(int length) {
+      if (length <= 2)
+         return;
+      length -= 1;
+      pathGradeY.reset();
+      for (int i = 1; i < (length); i++) {
+         float passo = (heightBottom - heightTop)/length;
+         pathGradeY.moveTo(widthLeft, heightTop + i * passo);
+         pathGradeY.lineTo(widthRight, heightTop + i * passo);
 
-      for (int i = 1; i < 18; i++) {
-         float passo = (widthRight - widthLeft)/18;
-         pathGrade.moveTo(widthLeft + i * passo, heightTop);
-         pathGrade.lineTo(widthLeft + i * passo, heightBottom);
-
-         pathTicker.moveTo(widthLeft + i * passo, heightBottom + paddingTicker);
-         pathTicker.lineTo(widthLeft + i * passo, heightBottom - paddingTicker);
+         pathTickerY.moveTo(widthLeft + paddingTicker, heightTop + i * passo);
+         pathTickerY.lineTo(widthLeft - paddingTicker, heightTop + i * passo);
       }
+   }
+
+   public void setGradeStatus(boolean status) {
+      this.gradeStatus = status;
+   }
+   public void setXNameTickers(String[] nameTickers) {
+      desenhaGradeX(nameTickers.length);
+      xNameTickers = nameTickers;
+   }
+   public void setYNameTickers(String[] nameTickers) {
+      desenhaGradeY(nameTickers.length);
+      yNameTickers = nameTickers;
+   }
+   public void setAxisTitles(String xTitle, String yTitle) {
+      this.xTitle = xTitle;
+      this.yTitle = yTitle;
    }
 }
